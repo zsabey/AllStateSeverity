2bd73583cf50a31b436226e97d59bc54fbab2695
Zach <zacharysabey@gmail.com>
2023-12-07 10:49:31 -0700

    new models

--- AllStateSeverityBoost.R
+++ AllStateSeverityBoost.R
@@ -3,40 +3,40 @@ library(tidyverse)
 library(tidymodels)
 library(embed)
 library(themis)
-
-##431498
-
+library(vroom)
 
 cl <- parallel::makePSOCKcluster(5)
 doParallel::registerDoParallel(cl)
 
+train <- vroom('train.csv')
+test <- vroom('test.csv')
+#skimr::skim(train)
+### EDA #### 
+#hist(train$loss)
+### Some variables are highly correlated to each other 
+#plot_correlation(train, type = 'continuous')
+train$loss <- log(train$loss)
+### Initial_Split ####
+boost <- boost_tree(mode = 'regression', 
+                    learn_rate = tune(),
+                    tree_depth = tune(),
+                    min_n = tune()
+) %>%
+  set_engine('xgboost', objective = 'reg:absoluteerror')
+recipe <- recipe(loss ~ ., train) %>% 
+  update_role(id, new_role = 'ID') %>%
+  step_scale(all_numeric_predictors()) %>%
+  step_corr(all_numeric_predictors(), threshold = .8) %>% 
+  step_novel(all_nominal_predictors()) %>%
+  step_unknown(all_nominal_predictors()) %>%
+  step_dummy(all_nominal_predictors()) %>% 
+  prep()
 
-trainCsv <- read_csv("train.csv") %>%
-  mutate_at(vars(cat1:cat116), as.factor)
 
-testCsv <- read_csv("test.csv") %>%
-  mutate_at(vars(cat1:cat116), as.factor)
+prepped <- prep(boost_recipe)
+baked <- bake(prepped, new_data = NULL)
+baked
 
-
-#Create the recipe and bake it
-
-boost_recipe <- recipe(loss ~ ., data=trainCsv) %>%
-  #step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(loss))%>%
-  step_zv() %>%
-  step_normalize(all_predictors()) #%>%
-  #step_pca(all_predictors(), threshold = .01)
-#step_smote(all_outcomes(), neighbors=5)
-
-
-
-
-#Set up the model
-boost_model <- boost_tree(tree_depth=tune(),
-                         trees=tune(),
-                         learn_rate=tune()) %>%
-  set_engine("xgboost") %>% #or "xgboost" but lightgbm is faster
-  set_mode("regression")
 
 ## Create a workflow with model & recipe
 boost_workflow <- workflow() %>%
@@ -51,7 +51,7 @@ boost_workflow <- workflow() %>%
 tuning_grid <- grid_regular(tree_depth(),
                             trees(),
                             learn_rate(),
-                            levels = 3)## L^2 total tuning possibilities
+                            levels = 5)## L^2 total tuning possibilities
 
 ## Set up K-fold CV
 folds <- vfold_cv(trainCsv, v = 3, repeats=1)
--- AllStateSeverityRF.R
+++ AllStateSeverityRF.R
@@ -17,12 +17,14 @@ testCsv <- read_csv("test.csv") %>%
 
 #Create the recipe and bake it
 
-rf_recipe <- recipe(loss ~ ., data=trainCsv) %>%
-  #step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(loss)) %>%
-  step_normalize(all_predictors()) %>%
-  step_pca(all_predictors(), threshold = .01)#%>%
-#step_smote(all_outcomes(), neighbors=5)
+rf_recipe <- recipe(loss ~ ., train) %>% 
+  update_role(id, new_role = 'ID') %>%
+  step_scale(all_numeric_predictors()) %>%
+  step_corr(all_numeric_predictors(), threshold = .6) %>% 
+  step_novel(all_nominal_predictors()) %>%
+  step_unknown(all_nominal_predictors()) %>%
+  step_dummy(all_nominal_predictors()) %>% 
+  prep()
 
 prep <- prep(rf_recipe)
 baked <- bake(prep, new_data = NULL)
@@ -32,60 +34,61 @@ baked
 
 #Set up the model
 my_mod <- rand_forest(mtry = 5,
-                      min_n=20,
-                      trees=500) %>%
+                      min_n=2,
+                      trees=1000) %>%
   set_engine("ranger") %>%
   set_mode("regression")
 
 ## Create a workflow with model & recipe
-rf_workflow <- workflow() %>%
+rf_wf <- workflow() %>%
   add_recipe(rf_recipe) %>%
-  add_model(my_mod)
+  add_model(my_mod) %>%
+  fit(data=trainCsv)
   
   
   
   ## Set up grid of tuning values
   
   #CV Results 1,23
-tuning_grid <- grid_regular(mtry(c(1,5)),
-                            min_n(),
-                              levels = 5)## L^2 total tuning possibilities
+# tuning_grid <- grid_regular(mtry(c(1,5)),
+#                             min_n(),
+#                               levels = 5)## L^2 total tuning possibilities
+# 
+# ## Set up K-fold CV
+# folds <- vfold_cv(trainCsv, v = 3, repeats=1)
+# 
+# ## Run the CV
+# CV_results <- rf_workflow %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#                  metrics=metric_set(mae)) #Or leave metrics NULL
+# 
+# ## Find best tuning parameters
+# collect_metrics(CV_results) %>% # Gathers metrics into DF
+#   filter(.metric=="mae") %>%
+#   ggplot(data=., aes(x=mtry, y=min_n, color=factor(mtry))) +
+#   geom_line()
+# 
+# collect_metrics(CV_results)
+# 
+# CV_results
+# ## Find Best Tuning Parameters
+# bestTune <- CV_results %>%
+#   select_best("mae")
+# bestTune
+# 
+# ## Finalize the Workflow & fit it
+# final_wf <- rf_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=trainCsv)
 
-## Set up K-fold CV
-folds <- vfold_cv(trainCsv, v = 3, repeats=1)
-
-## Run the CV
-CV_results <- rf_workflow %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-                 metrics=metric_set(mae)) #Or leave metrics NULL
-
-## Find best tuning parameters
-collect_metrics(CV_results) %>% # Gathers metrics into DF
-  filter(.metric=="mae") %>%
-  ggplot(data=., aes(x=mtry, y=min_n, color=factor(mtry))) +
-  geom_line()
-
-collect_metrics(CV_results)
-
-CV_results
-## Find Best Tuning Parameters
-bestTune <- CV_results %>%
-  select_best("mae")
-bestTune
-
-## Finalize the Workflow & fit it
-final_wf <- rf_workflow %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=trainCsv)
-
-rf_predictions <- final_wf %>%
+rf_predictions <- rf_wf %>%
   predict(new_data = testCsv)
 
 Sub1 <- rf_predictions %>% 
   bind_cols(testCsv) %>% 
-  select(id,.pred_1) %>%
-  rename(loss = .pred_1)
+  select(id,.pred) %>%
+  rename(loss = .pred)
 
 
 write_csv(Sub1, "RFSubmission.csv")
